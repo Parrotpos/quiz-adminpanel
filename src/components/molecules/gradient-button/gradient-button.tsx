b429c50d121541c2277d3ac52a498fa31546f9a8
HarshPatel8983 <patelharsh8983@gmail.com>
2025-09-12 13:35:51 +0530

    - button onclick dsable

--- src/components/molecules/gradient-button/gradient-button.tsx
+++ src/components/molecules/gradient-button/gradient-button.tsx
@@ -4,19 +4,32 @@ import React from "react";
 
 const GradientButton = ({
   children,
-  className,
+  className = "",
   title = "Button",
   fromGradient = "from-[#0E76BC]",
   toGradient = "to-[#283891]",
-  disabled,
-  loading,
+  disabled = false,
+  loading = false,
+  onClick,
   ...props
 }: IGradientButtonProps) => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (disabled || loading) {
+      e.preventDefault();
+      return;
+    }
+
+    if (onClick) {
+      onClick(e);
+    }
+  };
+
   return (
     <Button
       className={`h-10 bg-gradient-to-r ${fromGradient} ${toGradient} text-white border-none hover:opacity-90 cursor-pointer ${className}`}
-      {...props}
       disabled={disabled || loading}
+      onClick={handleClick}
+      {...props}
     >
       {children}
       {loading && <CustomLoader size={20} color="white" />}
